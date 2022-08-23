3927ed6c33720cafa57f929049633131c6aa103b
joseppbenvenuto@gmail.com <joseppbenvenuto@gmail.com>
2022-08-23 04:01:37 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,26 @@
+-- SELECT *, ORDERSELLINGPRICE - ORDERCOSTPRICE AS ORDERPROFIT
+-- FROM ANALYTICS.DBT_JBENVENUTO.RAW_ORDERS
+
+-- Reference from the models table instead of SFDB
+SELECT
+-- From raw_orders
+ord.ORDERID,
+ord.ORDERDATE,
+ord.SHIPDATE,
+ord.SHIPMODE,
+ord.ORDERSELLINGPRICE - ORDERCOSTPRICE AS ORDERPROFIT,
+ord.ORDERCOSTPRICE,
+ord.ORDERSELLINGPRICE,
+-- From raw_customers
+cus.CUSTOMERNAME,
+cus.SEGMENT,
+cus.COUNTRY,
+-- From raw_product
+prod.CATEGORY,
+prod.PRODUCTNAME,
+prod.SUBCATEGORY
+FROM {{ ref('raw_orders') }} as ord
+LEFT JOIN  {{ ref('raw_customers') }} AS cus
+ON ord.CUSTOMERID = cus.CUSTOMERID
+LEFT JOIN {{ ref('raw_products') }} AS prod
+ON ord.PRODUCTID = prod.PRODUCTID
